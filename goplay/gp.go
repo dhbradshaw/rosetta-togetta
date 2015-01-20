8f9f993d31acf1c25cbfa1ef3439a62bc05d2f92
Douglas H. Bradshaw <dhbradshaw@gmail.com>
2015-01-20 11:34:08 -0600

    added goplay to play with go concepts outside the main application.  Today I was playing with splitting up string files.

---
+++ goplay/gp.go
@@ -0,0 +1,14 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import ( 
+	"fmt"
+	"strings"
+)
+
+
+func main() {
+	greeting:="Hello world!"
+	fmt.Printf("%q\n",strings.Split(greeting," "))
+}
